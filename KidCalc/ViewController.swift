a9f62a44b57c3a6cad8459df3fbec7046f63787d
Emre Dogan <emredogan@outlook.com>
2022-04-02 20:20:12 +0200

    Added animations and sound on the button. Design fixes.

--- KidCalc/ViewController.swift
+++ KidCalc/ViewController.swift
@@ -6,8 +6,12 @@
 //
 
 import UIKit
+import AVFoundation
+
 
 class ViewController: UIViewController {
+    var audioPlayer: AVAudioPlayer!
+
     
     @IBOutlet var imageButtons: [UIButton]!
     
@@ -23,7 +27,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         setupImageTypeForButtons()
-
+        
         
     }
     
@@ -33,47 +37,47 @@ class ViewController: UIViewController {
     
     @IBAction func twoClicked(_ sender: Any) {
         addToOperations(value: "2")
-
+        
     }
     
     @IBAction func threeClicked(_ sender: Any) {
         addToOperations(value: "3")
-
+        
     }
     
     @IBAction func fourClicked(_ sender: Any) {
         addToOperations(value: "4")
-
+        
     }
     
     @IBAction func fiveClicked(_ sender: Any) {
         addToOperations(value: "5")
-
+        
     }
     
     @IBAction func sixClicked(_ sender: Any) {
         addToOperations(value: "6")
-
+        
     }
     
     @IBAction func sevenClicked(_ sender: Any) {
         addToOperations(value: "7")
-
+        
     }
     
     @IBAction func eightClicked(_ sender: Any) {
         addToOperations(value: "8")
-
+        
     }
     
     @IBAction func nineClicked(_ sender: Any) {
         addToOperations(value: "9")
-
+        
     }
     
     @IBAction func zeroClicked(_ sender: Any) {
         addToOperations(value: "0")
-
+        
     }
     
     @IBAction func equalClicked(_ sender: Any) {
@@ -89,7 +93,7 @@ class ViewController: UIViewController {
             self.present(alert, animated: true, completion: nil)
         }
         
-
+        
     }
     
     @IBAction func eraseClicked(_ sender: Any) {
@@ -97,45 +101,45 @@ class ViewController: UIViewController {
     }
     
     func validInput() ->Bool
+    {
+        var count = 0
+        var funcCharIndexes = [Int]()
+        
+        for char in operations
         {
-            var count = 0
-            var funcCharIndexes = [Int]()
-            
-            for char in operations
+            if(specialCharacter(char: char))
             {
-                if(specialCharacter(char: char))
-                {
-                    funcCharIndexes.append(count)
-                }
-                count += 1
+                funcCharIndexes.append(count)
             }
-            
-            var previous: Int = -1
-            
-            for index in funcCharIndexes
-            {
-                if(index == 0)
-                {
-                    return false
-                }
-                
-                if(index == operations.count - 1)
-                {
-                    return false
-                }
-                
-                if (previous != -1)
-                {
-                    if(index - previous == 1)
-                    {
-                        return false
-                    }
-                }
-                previous = index
-            }
-            
-            return true
+            count += 1
         }
+        
+        var previous: Int = -1
+        
+        for index in funcCharIndexes
+        {
+            if(index == 0)
+            {
+                return false
+            }
+            
+            if(index == operations.count - 1)
+            {
+                return false
+            }
+            
+            if (previous != -1)
+            {
+                if(index - previous == 1)
+                {
+                    return false
+                }
+            }
+            previous = index
+        }
+        
+        return true
+    }
     
     func specialCharacter(char: Character) -> Bool {
         if(char == "*") {
@@ -167,12 +171,12 @@ class ViewController: UIViewController {
     
     @IBAction func divisionClicked(_ sender: Any) {
         addToOperations(value: "/")
-
+        
     }
     
     @IBAction func multiplyClicked(_ sender: Any) {
         addToOperations(value: "*")
-
+        
     }
     
     @IBAction func addClicked(_ sender: Any) {
@@ -181,7 +185,7 @@ class ViewController: UIViewController {
     
     @IBAction func minusClicked(_ sender: Any) {
         addToOperations(value: "-")
-
+        
     }
     
     func clearAll() {
@@ -200,7 +204,75 @@ class ViewController: UIViewController {
             button.contentHorizontalAlignment = .fill
             button.contentVerticalAlignment = .fill
             button.imageView?.contentMode = .scaleAspectFit
+            
+            button.addTarget(self, action: #selector(animate), for: .touchUpInside)
+            
         }
+    
+    }
+    
+    @objc func animate(button: UIButton) {
+        button.shake(duration: 0.5, values: [-12.0, 12.0, -12.0, 12.0, -6.0, 6.0, -3.0, 3.0, 10.0])
+        playAudioAsset("button_sound")
+        
+        
+    }
+    
+    func playAudioAsset(_ assetName : String) {
+          guard let audioData = NSDataAsset(name: assetName)?.data else {
+             fatalError("Unable to find asset \(assetName)")
+          }
+
+          do {
+             audioPlayer = try AVAudioPlayer(data: audioData)
+             audioPlayer.play()
+          } catch {
+             fatalError(error.localizedDescription)
+        }
+    }
+    
+    
+}
+
+extension UIView {
+    
+    
+    // Using CAMediaTimingFunction
+    func shake(duration: TimeInterval = 0.5, values: [CGFloat]) {
+        let animation = CAKeyframeAnimation(keyPath: "transform.translation.x")
+        
+        // Swift 4.2 and above
+        animation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.linear)
+        
+        // Swift 4.1 and below
+        animation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.linear)
+        
+        
+        animation.duration = duration // You can set fix duration
+        animation.values = values  // You can set fix values here also
+        self.layer.add(animation, forKey: "shake")
+    }
+    
+    
+    // Using SpringWithDamping
+    func shake(duration: TimeInterval = 0.5, xValue: CGFloat = 12, yValue: CGFloat = 0) {
+        self.transform = CGAffineTransform(translationX: xValue, y: yValue)
+        UIView.animate(withDuration: duration, delay: 0, usingSpringWithDamping: 0.4, initialSpringVelocity: 1.0, options: .curveEaseInOut, animations: {
+            self.transform = CGAffineTransform.identity
+        }, completion: nil)
+        
+    }
+    
+    
+    // Using CABasicAnimation
+    func shake(duration: TimeInterval = 0.05, shakeCount: Float = 6, xValue: CGFloat = 12, yValue: CGFloat = 0){
+        let animation = CABasicAnimation(keyPath: "position")
+        animation.duration = duration
+        animation.repeatCount = shakeCount
+        animation.autoreverses = true
+        animation.fromValue = NSValue(cgPoint: CGPoint(x: self.center.x - xValue, y: self.center.y - yValue))
+        animation.toValue = NSValue(cgPoint: CGPoint(x: self.center.x + xValue, y: self.center.y - yValue))
+        self.layer.add(animation, forKey: "shake")
     }
     
 }
