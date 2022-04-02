a283d70ea05b674e83b8f3a2a57797980e5cd1ed
Emre Dogan <emredogan@outlook.com>
2022-04-02 18:25:45 +0200

    Initial commit with basic design completed

--- KidCalc/ViewController.swift
+++ KidCalc/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var imageButtons: [UIButton]!
+    
+    var operations: String = ""
+    
+    @IBOutlet weak var calculatorOperations: UILabel!
+    @IBOutlet weak var calculatorResult: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setupImageTypeForButtons()
+        
     }
-
-
+    
+    func setupImageTypeForButtons() {
+        for button in self.imageButtons {
+            print("CHANGING")
+            button.contentHorizontalAlignment = .fill
+            button.contentVerticalAlignment = .fill
+            button.imageView?.contentMode = .scaleAspectFit
+        }
+    }
+    
 }
 
